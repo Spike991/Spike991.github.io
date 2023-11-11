393f6f44db97efbe10bcfb8d4fe35398d26f7d1f
Alessio Cerullo <alessio.cerullo@finnova.com>
2023-11-11 07:45:58 +0100

    linking to cv works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CurriculumvitaeComponent} from './curriculumvitae/curriculumvitae.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'curriculumvitae', component: CurriculumvitaeComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
